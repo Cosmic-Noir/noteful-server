87ce393778fb8aedb885182f99fc21576a3a657a
Kristen Doucette <cosmicnoir90@gmail.com>
2019-10-12 13:59:16 -0600

    Add seeds for folders and notes

---
+++ seeds/seed.noteful_folders.sql
@@ -0,0 +1,5 @@
+INSERT INTO noteful_folders (folder_name)
+VALUES
+('Important'), 
+('Work Related'),
+('Wedding Checklist');
---
+++ seeds/seed.noteful_notes.sql
@@ -0,0 +1,8 @@
+INSERT INTO noteful_notes (title, content, folder_id)
+VALUES 
+('VERY IMPORTANT', 'This is very important stuff and things and what not that is now noted', 1),
+('OTHER Important Stuff', 'Do not forget about this other really important note!', 1),
+('Get New Job', 'Keep doing coding stuff and learn more and search for new job', 2),
+('Keep Going', 'You are almost there!! Keep pushing!!!', 2),
+('Thank You Cards', 'We really need to finish these and send them out', 3),
+('Photographs', 'Still awaiting photos from our photographer, should reach out', 3);
